630c118e5d3578c5844a59b4bf73191d40eab43b
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-10-24 12:34:05 -0600

    commit

--- src/ChoosingQuestion.java
+++ src/ChoosingQuestion.java
@@ -5,14 +5,16 @@ import java.util.Random;
 
 public class ChoosingQuestion 
 {
-	public static void choosing(ArrayList <FlashCard> trivia, ArrayList <FlashCard> math, int questionNumber)
+	static int questionNumber;
+	
+	public static void choosing()
 	{
 		Scanner intInput = new Scanner(System.in);
 		Scanner stringInput = new Scanner(System.in);
 		
 		
 		System.out.println("What flahscards do you want to study first?\n1. Trivia\n2. Math\n");
-		
+		questionNumber = pickRandom();
 		int chooseQuestions = intInput.nextInt();
 		String answer;
 		int counterCorrect = 0;
@@ -21,26 +23,28 @@ public class ChoosingQuestion
 		
 		if(chooseQuestions == 1)
 		{	 
-			System.out.println(trivia.get(questionNumber).getQuestion());
-			for (int i = trivia.size(); i > 0; i--)
+			System.out.println(FlashCardRunner.trivia.get(questionNumber).getQuestion());
+			
+			for (int i = FlashCardRunner.trivia.size(); i > 0; i--)
 			{
 				
 				answer = stringInput.nextLine();			
 				
-				if(answer.equals(trivia.get(questionNumber).getAnswer()))
+				if(answer.equals(FlashCardRunner.trivia.get(questionNumber).getAnswer()))
 				{
 					counterCorrect = counterCorrect + 1;
 					System.out.println("Number got correct: " + counterCorrect + "\n"); 	
-					trivia.remove(questionNumber);
+					FlashCardRunner.trivia.remove(questionNumber);
 					
-					if(trivia.isEmpty() == true)
+					if(FlashCardRunner.trivia.isEmpty() == true)
 					{	
 						System.out.println("You have answered all of the questions.");
 					}
 					
 					else
 					{
-						System.out.println("That is correct. Here is a new question\n" + trivia.get(questionNumber).getQuestion());
+						pickRandom();
+						System.out.println("That is correct. Here is a new question\n" + FlashCardRunner.trivia.get(questionNumber).getQuestion());
 					}	
 				}
 		
@@ -55,18 +59,18 @@ public class ChoosingQuestion
 					else
 					{ 	
 						
-						if(trivia.isEmpty() == true)
+						if(FlashCardRunner.trivia.isEmpty() == true)
 						{
-							trivia.remove(questionNumber);
+							FlashCardRunner.trivia.remove(questionNumber);
 							System.out.println("You have answered all of the questions.");
 						}
 						
 						else
 						{
 							System.out.println("Again, that is incorrect. The correct answer is: " 
-							+ trivia.get(questionNumber).getAnswer()+ ".\n");
-							trivia.remove(questionNumber);
-							System.out.println("Here is a new question.\n" + trivia.get(questionNumber).getQuestion());
+							+ FlashCardRunner.trivia.get(questionNumber).getAnswer()+ ".\n");
+							FlashCardRunner.trivia.remove(questionNumber);
+							System.out.println("Here is a new question.\n" + FlashCardRunner.trivia.get(questionNumber).getQuestion());
 							
 						}	
 					}
@@ -76,31 +80,27 @@ public class ChoosingQuestion
 		
 		else if(chooseQuestions == 2)
 		{
-			System.out.println(math.get(questionNumber).getQuestion());
+			System.out.println(FlashCardRunner.math.get(questionNumber).getQuestion());
 			answer = stringInput.nextLine();			
 			
-			if(answer.equals(math.get(questionNumber).getAnswer()))
+			if(answer.equals(FlashCardRunner.math.get(questionNumber).getAnswer()))
 			{
 				System.out.println("That is correct.");
 			}
 			else
 			{
-				System.out.println("That is incorrect. The correct answer is: " + math.get(questionNumber).getAnswer() + ".\n");
+				System.out.println("That is incorrect. The correct answer is: " + FlashCardRunner.math.get(questionNumber).getAnswer() + ".\n");
 			}
 		}
 	}
 
-	public static void pickRandom(int questionNumber, ArrayList <FlashCard> trivia, ArrayList <FlashCard> math) 
+	public static int pickRandom() 
 	{
-		if(questionNumber >= trivia.size())
+		questionNumber = (int)(Math.random()*(FlashCardRunner.trivia.size()));
+		if(questionNumber >= FlashCardRunner.trivia.size())
 		{
 			pickRandom();
 		}
-		
-	}
-
-	public static int pickRandom() 
-	{
-		return (0);  	
+		return (questionNumber);
 	}
 }
